c4386e1ff1a3b8222629ff1fbc18c183dce91326
datmaster2004 <datmaster200418@gmail.com>
2025-04-11 08:54:37 +0700

    feature<BE>ntd: create Base Service

---
+++ SchoolManagerApp/src/Service/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagerApp.src.Service
+{
+    internal class BaseService
+    {
+        public interface IBaseService<T> where T : class
+        {
+            Task<IEnumerable<T>> GetAllAsync();
+            Task<T> CreateAsync(T entity);
+            Task<bool> DeleteAsync(int id);
+        }
+    }
+}
