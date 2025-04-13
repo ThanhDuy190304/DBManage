c7dfe1251fcb972e17365859b0d1889c7fcbb20b
dnquocdat <dangngocquocdat10112004@gmail.com>
2025-04-13 23:23:50 +0700

    Feature<BE>Quocdat: Connection

---
+++ SchoolManagerApp/src/Controllers/Form1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagerApp.src.Service;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace SchoolManagerApp.src.Controllers
+{
+    public class Form1Controller
+    {
+        private DatabaseService _databaseService;
+
+        public Form1Controller()
+        {
+            _databaseService = new DatabaseService();
+        }
+
+        public void LoadData(DataGridView dataGridView)
+        {
+            try
+            {
+                string query = "SELECT * FROM DONVI";
+                DataTable dataTable = _databaseService.ExecuteQuery(query);
+                dataGridView.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nInner Exception: " + ex.InnerException?.Message);
+            }
+        }
+    }
+}
---
+++ SchoolManagerApp/src/Service/DatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace SchoolManagerApp.src.Service
+{
+    public class DatabaseService
+    {
+        private string _connectionString = "User Id=admin;Password=Admin123;Data Source=localhost:1521/QL_NoiBo";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(_connectionString))
+                {
+                    OracleCommand cmd = new OracleCommand(query, connection);
+                    OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while executing the query: " + ex.Message);
+            }
+        }
+    }
+}
